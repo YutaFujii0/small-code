534e9e390bd95449975c44eabbfffafe971aa6de
yuta.fujii <yuta.fujii@whoami.local>
2021-09-30 17:23:02 +0900

    Ruby TypeProf

---
+++ ruby/typeprof/README.md
@@ -0,0 +1,13 @@
+
+
+```
+$ rbenv init
+
+$ rbenv local 3.0.2
+
+$ rbenv shell 3.0.2
+
+$ gem install typeprof
+
+$ typeprof test.rb
+```
---
+++ ruby/typeprof/test.rb
@@ -0,0 +1,7 @@
+class User
+  def initialize(name:, age:)
+    @name, @age = name, age
+  end
+  attr_reader :name, :age
+end
+User.new(name: "John", age: 20)
