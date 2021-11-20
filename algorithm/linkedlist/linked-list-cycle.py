eea44baac59c58b906fc73ed63e7948b8dd4c8db
YutaFujii0 <y.f.mar2@gmail.com>
2021-11-20 11:32:39 +0900

    Floyd's Cycle Detection Algorithm: solution refined

--- algorithm/linkedlist/linked-list-cycle.py
+++ algorithm/linkedlist/linked-list-cycle.py
@@ -1,21 +1,13 @@
+from typing import Optional
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-# pos is only used in explanation, not given as arguments or needed to return
-# naively thought, every time check if next has already traversed.
-# store node
-# move to next
-# check if node is already stored
-# if it is, return true
-# else move next
-# iterate above
-# return false
-# time complexity: sigma(k * (k-1)) = O(n^2)
-# space complexity: O(n) for store
 
+# My First Solution
 # create special node
 # get pointer to next node
 # set next node of current traversal to special node
@@ -35,3 +27,24 @@ class Solution:
             next_node.next = special_node
             next_node = tmp
         return next_node == special_node
+
+
+# Best Practice Solution
+# Floyd’s Cycle Detection Algorithm.
+# ... This algorithm is applied when you want to determine
+# where a data structure(graph, linkedlist, etc) has cycle
+# cf https://iq.opengenus.org/fast-and-slow-pointer-technique/
+# Set two pointers(slow, fast)
+# slow will be advanced one step each,
+# whereas fast moves more than one step
+# if both pointer points the same address, that means a cycle exists
+class Solution2:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
