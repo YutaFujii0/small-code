9b595f8333af2d8328d4cb80ba0398d59cc110ff
YutaFujii0 <y.f.mar2@gmail.com>
2021-11-25 22:37:38 +0900

    Algo kth largest emelent: using heap

--- algorithm-datastructure/sort/kth-largest-element-in-a-stream.py
+++ algorithm-datastructure/sort/kth-largest-element-in-a-stream.py
@@ -1,55 +1,77 @@
 from typing import List
-# keep nums fully sorted
-# - merge sort at initialization?
-# - sort(how?) every addition?
-#  - if data is array or linked list, insertion need O(n) time
-
-# heap is only top max/min value ensured, not kth sorted value
+# Heap structure
+# to keep track of kth LARGEST number among n,
+# set MIN heap that has only k element(and implement it with array)
+# every insertion, add element to heap and heapify
+# to return kth largetst element, read first element of heap
+# (not polling)
+# __init__()
+# - store all element in heap(List[int])
+# - set size
+# - until size == k:
+#   - heap.poll()
+#   - size --
+# add()
+# - add element to heap
+# - heapify
+# - poll()
+# - read first element
 
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.sorted_nums = self.merge_sort(nums)
+        # self.nums = nums
         self.k = k
+        self.heap = []
+        self.size = 0
+        for num in nums:
+            self.add(num)
 
     def add(self, val: int) -> int:
-        i = 0
-        while i < len(self.sorted_nums) and self.sorted_nums[i] >= val:
-            i += 1
-        self.sorted_nums = self.sorted_nums[:i] + [val] + self.sorted_nums[i:]
-        return self.sorted_nums[self.k - 1]
+        if len(self.heap) == 0 or val > self.heap[0] or self.size < self.k:
+            self.heap.append(val)
+            self.size += 1
+            self.heapify_up()
+            while self.size > self.k:
+                self.heap_poll()
+        return self.heap[0]
 
-    def merge_sort(self, nums: List[int]) -> List[int]:
-        if len(nums) < 2:
-            return nums
+    def heapify_up(self):
+        child_index = self.size - 1
+        parent_index = (child_index - 1) // 2
+        while child_index != 0 and self.heap[parent_index] > self.heap[child_index]:
+            tmp = self.heap[parent_index]
+            self.heap[parent_index] = self.heap[child_index]
+            self.heap[child_index] = tmp
+            child_index = parent_index
+            parent_index = (child_index - 1) // 2
 
-        left = nums[:len(nums) // 2]
-        right = nums[len(nums) // 2:]
-        return self.merge(self.merge_sort(left), self.merge_sort(right))
+    def heapify_down(self):
+        parent_index = 0
+        left_child_index = parent_index * 2 + 1
+        while left_child_index < self.size:
+            smallest_child_index = left_child_index
+            if left_child_index + 1 < self.size:
+                if self.heap[left_child_index] > self.heap[left_child_index + 1]:
+                    smallest_child_index += 1
 
-    def merge(self, left: List[int], right: List[int]) -> List[int]:
-        merged_list = []
-        while len(left) != 0 and len(right) != 0:
-            if left[0] < right[0]:
-                merged_list.append(right.pop(0))
-            else:
-                merged_list.append(left.pop(0))
-        if len(left) > 0:
-            merged_list += left
-        if len(right) > 0:
-            merged_list += right
-        return merged_list
+            if self.heap[parent_index] > self.heap[smallest_child_index]:
+                tmp = self.heap[parent_index]
+                self.heap[parent_index] = self.heap[smallest_child_index]
+                self.heap[smallest_child_index] = tmp
+            parent_index = smallest_child_index
+            left_child_index = parent_index * 2 + 1
 
+    def heap_poll(self) -> Optional[int]:
+        if self.size == 0:
+            return None
+        if self.size == 1:
+            return self.heap.pop()
 
-# Your KthLargest object will be instantiated and called as such:
-k = 1
-nums = []
-nums = [1,45,3,6]
-obj = KthLargest(k, nums)
-print(obj.sorted_nums)
-param_1 = obj.add(9)
-print(obj.sorted_nums)
-print(param_1)
-param_1 = obj.add(9)
-print(obj.sorted_nums)
-print(param_1)
+        head = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop(-1)
+        self.size -= 1
+        self.heapify_down()
+
+        return head
