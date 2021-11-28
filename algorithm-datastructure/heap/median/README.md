2fcf7835af0cf7f40b4ab6da6e05b37619fd81b7
YutaFujii0 <y.f.mar2@gmail.com>
2021-11-28 21:19:19 +0900

    Algo: Median Maintenance using heaps

---
+++ algorithm-datastructure/heap/median/README.md
@@ -0,0 +1,25 @@
+# Programming Assignment
+
+The goal of this problem is to implement the "Median Maintenance" algorithm.
+The text file contains a list of the integers from 1 to 10000 in unsorted order; you should treat this as a stream of numbers, arriving one by one.
+if k is odd, then m_k is ((k+1)/2)th smallest number among x_1,...,x_k;
+if k is even, then m_k is the (k/2)th smallest number among x_1,...,x_k.
+In the box below you should type the sum of these 10000 medians, modulo 10000.
+
+OPTIONAL EXERCISE: Compare the performance achieved by heap-based and search-tree-based implementations of the algorithm.
+
+
+# Psuedocode
+
+Recap: the definition of median here is
+if k is odd, then m_k is ((k+1)/2)th smallest number among x_1,...,x_k.
+if k is even, then m_k is the (k/2)th smallest number among x_1,...,x_k.
+
+- Two heaps (S: smaller half of values are stored, L: larger half of values are stored resp)
+- On addition, if S[0] < val
+  - insert val to L
+- else
+  - insert val to S
+- until size(S) - size(L) is 0 or 1
+  - rebalance(poll and insert)
+- return head of S
---
+++ algorithm-datastructure/heap/median/src/main.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
+
+use datastructures::HeapNode;
+
+const FILE_PATH: &str = "./dataset.txt";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Median Maintenance!");
+
+    let mut median_sum = 0;
+    let mut smallers = HeapNode::<i32, _>::new(out_of_order_smallers);
+    let mut largers = HeapNode::<i32, _>::new(out_of_order_largers);
+    let lines = read_lines(FILE_PATH)?;
+    for line_result in lines {
+        if let Ok(line) = line_result {
+            let val = line.parse::<i32>();
+            if let Ok(value) = val {
+                match smallers.peak() {
+                    None => smallers.insert(value),
+                    Some(head) => {
+                        if head < &value {
+                            largers.insert(value);
+                        } else {
+                            smallers.insert(value);
+                        }
+                    }
+                }
+                while smallers.size < largers.size {
+                    smallers.insert(largers.poll().unwrap());
+                }
+                while smallers.size > largers.size + 1 {
+                    largers.insert(smallers.poll().unwrap());
+                }
+                median_sum += smallers.peak().unwrap();
+            }
+        }
+        println!("{:?}", smallers.peak());
+    }
+    println!("Iteration finished. Module 10000 of the sum of medians is {:?}", median_sum % 10000);
+    Ok(())
+}
+
+fn out_of_order_smallers(parent: &i32, child: &i32) -> bool {
+    parent < child
+}
+
+fn out_of_order_largers(parent: &i32, child: &i32) -> bool {
+    parent > child
+}
+
+fn read_lines<P>(path: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>
+{
+    let file = File::open(path)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
