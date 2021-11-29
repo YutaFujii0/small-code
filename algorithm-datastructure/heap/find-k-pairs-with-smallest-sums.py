f66cfbf491f95c47189f65729c775f235031fc11
YutaFujii0 <y.f.mar2@gmail.com>
2021-11-30 08:49:01 +0900

    Algo naively improve time complexity from n^2 to k^2

--- algorithm-datastructure/heap/find-k-pairs-with-smallest-sums.py
+++ algorithm-datastructure/heap/find-k-pairs-with-smallest-sums.py
@@ -4,7 +4,15 @@ import heapq
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
         heap = []
+        k_elements = []
+        i = 0
+        j = 0
         for n1 in nums1:
             for n2 in nums2:
                 heap.append((n1 + n2, [n1, n2]))
-        return [item[1] for item in heapq.nsmallest(k, heap)]
+                j += 1
+                if i + j > k:
+                    break
+            i += 1
+            j = 0
+        return [item[1] for item in nsmallest(k, heap)]
