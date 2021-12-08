a30fc7484065b4a50bdf340e22b2ac681a283d36
YutaFujii0 <y.f.mar2@gmail.com>
2021-12-08 20:45:14 +0900

    Algo word ladder using BFS

--- algorithm-datastructure/graph/word-ladder.py
+++ algorithm-datastructure/graph/word-ladder.py
@@ -2,14 +2,15 @@ from typing import List
 # basic concept: wordList is a graph
 # if wordList is formed into graph(vertix, edge), can I solve this?
 # dijkstra's shortest algorithm
+# or just BFS (since lenght of edge is all the same)
 # from begin word to end word
-# (in the loop, end word is come, done): 
+# (in the loop, end word is come, done):
 
 # n: vertices, m: edges
-# creating a graph:
-# dijkstra's setup: heap O(n)
-# find edge from begin node: ???
-# dijkstra's loop: m * O(logn)
+# Complexity
+# - time complexity
+#  - creating a graph: 
+#  - BFS: O(m*n)
 
 class Graph:
     def __init__(self, wordList: List[str]):
@@ -18,34 +19,40 @@ class Graph:
         for word in wordList:
             self.nodes.append((word, []))
 
-        def merge_sort(words):
-            if len(words) == 1:
-                return words
-            split = len(words) // 2
-            return merge(merge_sort(words[:split]), merge_sort(words[split:]))
-
-        def merge(left, right):
-            merged = []
-            while left and right:
-                if left[0][1] > right[0][1]:
-                    merged.append(right.pop(0))
-                else:
-                    merged.append(left.pop(0))
-            if left:
-                merged += left
-            if right:
-                merged += right
-            return merged
-
         for i in range(length):
             tmp_dic = [(node, node[0][:length-i-1]+node[0][length-i:]) for node in self.nodes]
-            tmp_dic = merge_sort(tmp_dic)
-            for idx in range(len(tmp_dic) - 1):
-                if tmp_dic[idx][1] == tmp_dic[idx+1][1]:
-                    tmp_dic[idx][0][1].append(tmp_dic[idx+1][0])
-                    tmp_dic[idx+1][0][1].append(tmp_dic[idx][0])
-        print(self.nodes)
+            tmp_hash = {}
+            for item in tmp_dic:
+                if item[1] in tmp_hash:
+                    tmp_hash[item[1]].append(item[0])
+                else:
+                    tmp_hash[item[1]] = [item[0]]
+            for key in tmp_hash:
+                for node in tmp_hash[key]:
+                    for connected_node in tmp_hash[key]:
+                        if node != connected_node:
+                            node[1].append(connected_node)
+
+    def traverse(self, endWord: str):
+        explored = {}
+        queue = [[self.nodes[0]]]
+        hops = 1
+        while queue and queue[0]:
+            nodes = queue.pop(0)
+            next_iter = []
+            for node in nodes:
+                if node[0] == endWord:
+                    return hops
+                if node[0] in explored:
+                    continue
+                explored[node[0]] = hops
+                next_iter += node[1]
+                
+            queue.append(next_iter)
+            hops += 1
+        return 0
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        Graph([beginWord] + wordList)
+        graph = Graph([beginWord] + wordList)
+        return graph.traverse(endWord)
