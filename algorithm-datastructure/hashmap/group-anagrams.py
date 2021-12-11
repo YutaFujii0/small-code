75b4c5eabbb7b88712584935bd6911cceab3a181
YutaFujii0 <y.f.mar2@gmail.com>
2021-12-11 12:51:48 +0900

    update another solution for group anagrams

--- algorithm-datastructure/hashmap/group-anagrams.py
+++ algorithm-datastructure/hashmap/group-anagrams.py
@@ -24,3 +24,32 @@ class Solution:
             else:
                 hash[sorted_word] = [word]
         return [hash[key] for key in hash.keys()]
+
+
+# Anagram words have in common counts of each letters
+# i.e) eat and tea has a:1, e:1, t:1
+# convert string into character count
+# look up if the same set exists, and group them
+# Hashtable
+# hash = {}
+# for str in strs
+#  - convert str to char-count
+#  - add to hash table
+# for item in hashtable
+#  - push value to return array
+# return array
+# Complexity:
+# - time complexity: n * O(1) + O(keys)
+# - space complexity: O(n) for hashtable (or even more to use hashtable)
+
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        alphabet_composition = {}
+        for word in strs:
+            key = [0]*26
+            for char in word:
+                key[ord(char) - 97] += 1
+            alphabet_composition.setdefault(tuple(key), []).append(word)
+        return [value for _, value in alphabet_composition.items()]
