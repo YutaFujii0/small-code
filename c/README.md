988d0a6818fc42851fa7933153d437a048cfbbd1
YutaFujii0 <y.f.mar2@gmail.com>
2021-12-30 10:13:01 +0900

    C: loop, pointer

---
+++ c/README.md
@@ -0,0 +1,18 @@
+# Some Tips for Programming in C
+
+1. Preprocessor Stuff
+2. Variable Declarations
+3. Type Definitions
+4. Function Definitions
+5. Statements & Expressions
+6. Comments
+
+# Type in C
+
+- Use `int8_t` or `uint64_t` (stdint.h) rather than `int` to avoid dependency to your system
+  - `int` usually 4bytes but some micro processor don't
+
+
+# Resources
+
+- Jacob Sorber[Youtube](https://www.youtube.com/watch?v=SC8uWXmDJs4&list=PL9IEJIKnBJjG5H0ylFAzpzs9gSmW_eICB)
--- c/hello.c
+++ c/hello.c
@@ -2,8 +2,34 @@
 
 int main() {
     int mynumber = 0;
-
     printf("Hello World %i\n", mynumber);
+
+    // pointer
+    int y = 7;
+    int *p = &y;
+    printf("%p, %i\n", p, *p);
+
+    // pointer index
+    int x[5] = {1,2,3,4,5};
+    int *p2 = x;
+    p2[0] = 100;
+    *(p2+2) = 200;
+    printf("x[0] = %i\n", x[0]);
+    printf("x[1] = %i\n", x[1]);
+    printf("x[2] = %i\n", x[2]);
+    printf("x[3] = %i\n", x[3]);
+    printf("x[4] = %i\n", x[4]);
+
+    // loop
+    for (int counter=0; counter < 5; counter++){
+        printf("for loop %i\n", x[counter]);
+    }
+
+    int do_count = 0;
+    do {
+        printf("This is inside do loop\n");
+        do_count++;
+    } while (do_count < 3);
 }
 
 // # ... preprocessor
