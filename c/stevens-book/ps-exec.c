8539c1cbd7f4ca9049f0db1063bb1982e553d3ff
YutaFujii0 <y.f.mar2@gmail.com>
2022-01-03 13:04:26 +0900

    C implement echoall

--- c/stevens-book/ps-exec.c
+++ c/stevens-book/ps-exec.c
@@ -11,14 +11,14 @@ main(void)
     if ((pid = fork()) < 0) {
         printf("fork error\n");
     } else if (pid == 0) {
-        if (execle("/usr/bin/printenv", "printenv", "myarg1", "MY arg2", (char *)0, env_init) < 0)
+        if (execle("./echoall", "echoall", "myarg1", "MY arg2", (char *)0, env_init) < 0)
             printf("execle error\n");
     }
 
     if ((pid = fork()) < 0) {
         printf("fork error\n");
     } else if (pid == 0) {
-        if (execlp("printenv", "printenv", "only 1 arg", (char *)0) < 0)
+        if (execlp("echoall", "echoall", "only 1 arg", (char *)0) < 0)
             printf("execlp error\n");
     }
 
