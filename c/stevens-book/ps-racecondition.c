013d704b86097bdd48da47062ba1006f2db11f6f
YutaFujii0 <y.f.mar2@gmail.com>
2022-01-03 11:05:52 +0900

    C wait and tellwait to avoid race condition

--- c/stevens-book/ps-racecondition.c
+++ c/stevens-book/ps-racecondition.c
@@ -6,12 +6,16 @@ int
 main(void)
 {
     pid_t pid;
+    TELL_WAIT();
+
     if ((pid = fork()) < 0) {
         err_sys("fork error");
     } else if (pid == 0) {
+        WAIT_PARENT();
         charatatime("output from child\n");
     } else {
         charatatime("output from parent\n");
+        TELL_CHILD(pid);
     }
     exit(0);
 }
