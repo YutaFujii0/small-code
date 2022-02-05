0ba8662c555d5d5d08d77f7c506246451d3cd286
Yuta Fujii <y.f.mar2@gmail.com>
2022-02-05 21:28:47 +0900

    increasing triplet subsequence

---
+++ algorithm-datastructure/others/increasing-triplet-subsequence.py
@@ -0,0 +1,25 @@
+# [5,6,4,7] -> true
+# [5,6,1,2,3] -> true
+# naive solution, nC3 combinations, O(n^3) time complexity
+
+# property: order does matter
+# 
+# idea: keep minimum 3 elements
+# [min1, min2]
+# if i > min2, return true
+# else if i > min1, min2= i
+# else min1 = i
+# n * O(3) time complexity
+# O(1) space complexity
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        min1, min2 = nums[0], pow(2,31)
+        for i in nums[1:]:
+            if i > min2:
+                return True
+            elif i > min1:
+                min2 = i
+            else:
+                min1 = i
+        return False
