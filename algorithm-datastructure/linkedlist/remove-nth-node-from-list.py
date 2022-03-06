488193e17dabcf3c985e24a379250e4ebc90b805
YutaFujii0 <y.f.mar2@gmail.com>
2022-03-07 08:14:50 +0900

    remove nth node from list, one pass solution

--- algorithm-datastructure/linkedlist/remove-nth-node-from-list.py
+++ algorithm-datastructure/linkedlist/remove-nth-node-from-list.py
@@ -36,3 +36,17 @@ class Solution:
             i += 1
 
         return head
+
+
+class Solution2:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummyNode = ListNode(0, head)
+        fast = dummyNode
+        slow = dummyNode
+        for i in range(n+1):
+            fast = fast.next
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummyNode.next
