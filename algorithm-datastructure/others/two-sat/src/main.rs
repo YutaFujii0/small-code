064e12aa32721bf0b0d05bbc830043c6e40dadea
YutaFujii0 <y.f.mar2@gmail.com>
2022-05-07 12:19:11 +0900

    Refactor: Use method over function

--- algorithm-datastructure/others/two-sat/src/main.rs
+++ algorithm-datastructure/others/two-sat/src/main.rs
@@ -3,20 +3,23 @@ mod node;
 mod parser;
 mod scc;
 
+use std::error::Error;
+
 use parser::Parser;
 use scc::*;
 
 const FILE_PATH: &str = "dataset-sample.txt";
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     println!("2SAT Problem using strongly connected components!");
     if let Ok(ret) = Parser::run(FILE_PATH) {
         let (data_count, (nodes, nodes_rev)) = ret;
-        let finishing_times = dfs_loop_first(&nodes_rev, data_count * 2);
-        let leaders = dfs_loop_second(&nodes, &finishing_times, data_count * 2);
+        let finishing_times = Solution::dfs_loop_first(&nodes_rev)?;
+        let leaders = Solution::dfs_loop_second(&nodes, &finishing_times)?;
 
         for i in 1..(data_count+1) {
             println!("{} {:?}", i, leaders.get(&(i*2)).unwrap_or(&0) != leaders.get(&(i*2-1)).unwrap_or(&1));
         }
     }
+    Ok(())
 }
