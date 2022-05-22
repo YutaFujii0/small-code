bead13cda0a7a0e289f41a6af120dc4bd9bfdd05
YutaFujii0 <y.f.mar2@gmail.com>
2022-05-22 14:34:47 +0900

    LRU cache(doubly linked list)

--- algorithm-datastructure/hashmap/lru-cache.py
+++ algorithm-datastructure/hashmap/lru-cache.py
@@ -20,34 +20,67 @@
 # - read hash table
 # - push front referenced key to queue if it exists
 
+class ListNode:
+    def __init__(self, k = None, v = None):
+        self.key = k
+        self.val = v
+        self.next = None
+        self.prev = None
+
+        
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.referencedDeque = deque()
+        self.head = ListNode()
+        self.tail = self.head
+        # key = ListNode
         self.cache = {}
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            if len(self.referencedDeque) == self.capacity:
-                self.referencedDeque.pop()
-            self.referencedDeque.appendleft(key)
-            return self.cache[key]
+            # move reference order
+            node = self.cache[key]
+            if node != self.tail:
+                node.prev.next = node.next
+                if node.next:
+                    node.next.prev = node.prev
+                self.tail.next = node
+                node.prev = self.tail
+                self.tail = self.tail.next
+
+            return self.cache[key].val
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache = value
-            if len(self.referencedDeque) == self.capacity:
-                self.referencedDeque.pop()
-            self.referencedDeque.appendleft(key)
+            self.cache[key].val = value
+            # move reference order
+            node = self.cache[key]
+            if node == self.tail:
+                return
+            node.prev.next = node.next
+            if node.next:
+                node.next.prev = node.prev
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = self.tail.next
         else:
-            if len(self.cache) == 2:
-                self.cache.pop(self.referencedDeque.pop(), None)
-            self.cache[key] = value
-            self.referencedDeque.appendleft(key)
+            if len(self.cache) == self.capacity:
+                self.cache.pop(self.head.next.key, None)
+                self.head.next = self.head.next.next
+                if self.head.next:
+                    self.head.next.prev = self.head
+                else:
+                    self.tail = self.head
+            new = ListNode(key, value)
+            self.cache[key] = new
+            # make referencing order
+            self.tail.next = new
+            new.prev = self.tail
+            self.tail = self.tail.next
 
 
 # Your LRUCache object will be instantiated and called as such:
