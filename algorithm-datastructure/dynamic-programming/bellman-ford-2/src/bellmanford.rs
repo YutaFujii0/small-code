e109979989f1782cdfa5cc432643078b90a79fe0
YutaFujii0 <y.f.mar2@gmail.com>
2022-06-05 21:28:51 +0900

    Implement Bellman ford algorith using my gcollection graph

--- algorithm-datastructure/dynamic-programming/bellman-ford-2/src/bellmanford.rs
+++ algorithm-datastructure/dynamic-programming/bellman-ford-2/src/bellmanford.rs
@@ -9,7 +9,7 @@ pub type RawData = Vec<(VertexValue, VertexValue, Cost)>;
 pub struct Solution;
 
 impl Solution {
-    pub fn call(edge_list: RawData) -> Result<Cost, Box<dyn Error>> {
+    pub fn call(edge_list: &RawData, source_index: usize) -> Result<(Vec<Cost>, bool), Box<dyn Error>> {
         let mut graph = AdjacencyMatrix::<VertexValue, Cost>::new();
 
         // scan edge list to collect nodes
@@ -32,7 +32,7 @@ impl Solution {
         // let A as 2D array
         let mut cost_matrix = vec![vec![i64::MAX; *n]; *n];
         // set initial value for A[0, s]
-        cost_matrix[0][0] = 0;
+        cost_matrix[0][source_index] = 0;
 
         // for i = 1.. n (nth iteration is for checking negative cycle)
         for i in 1..*n {
@@ -40,9 +40,25 @@ impl Solution {
             for j in 0..*n {
                 // A[i,v] = min (A[i-1, v], min(A[i-1, w] + c_wv))
                 let cost_of_last_iter = cost_matrix[i - 1][j];
-                // let min_cost_from_neighbor = graph.neighbors(j)
+                let min_cost_from_neighbor = graph.neighbors_to(j)
+                    .iter()
+                    .map(|&(vertex_id, weight)| -> Cost {
+                        let prev = cost_matrix[i - 1][vertex_id.value];
+                        if prev == Cost::MAX {
+                            prev
+                        } else {
+                            prev + weight
+                        }
+                    })
+                    .min()
+                    .ok_or_else(|| "Cannot take min from neighbors.")?;
+                if cost_of_last_iter > min_cost_from_neighbor {
+                    cost_matrix[i][j] = min_cost_from_neighbor
+                } else {
+                    cost_matrix[i][j] = cost_of_last_iter
+                }
             }
         }
-        Ok(1i64)
+        Ok((cost_matrix[n - 1].clone(), cost_matrix[n - 1] == cost_matrix[n - 2]))
     }
 }
--- algorithm-datastructure/dynamic-programming/bellman-ford-2/src/main.rs
+++ algorithm-datastructure/dynamic-programming/bellman-ford-2/src/main.rs
@@ -15,9 +15,25 @@ fn main() -> Result<(), Box<dyn Error>> {
         .ok_or(NoArgumentError)?;
     
 
+    let mut local_mins = vec![];
     if let Ok(res) = parse(path) {
-        println!("{:?}", res);
-        Solution::call(res);
+        let n = &res.iter()
+            .map(|item| item.0)
+            .max()
+            .ok_or_else(|| "Unable to find max index of node from argument.")?;
+
+        for source_index in 0..*n {
+            if let Ok((result, no_negative_cycle)) = Solution::call(&res, source_index) {
+                if no_negative_cycle {
+                    let min = result.into_iter().min().unwrap();
+                    local_mins.push(min);
+                } else {
+                    println!("Negative cost cycle detected! Stop iterating...");
+                    break
+                }
+            }
+        }
+        println!("shortest shortest path is {:?}", local_mins.iter().min().unwrap_or_else(|| &i64::MAX))
     }
     Ok(())
 }
