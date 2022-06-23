d5637a0654e01adb2c592ef46b6df1c57f9ec0dd
YutaFujii0 <y.f.mar2@gmail.com>
2022-06-23 16:05:04 +0900

    java basics expressions

---
+++ java/HelloWorld/expressions/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+        gameOver = true;
+        score = 800;
+        levelCompleted = 5;
+        bonus = 100;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position "
+                + highScorePosition + " on the high score table");
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            return finalScore;
+        }
+        return -1;
+    }
+}
