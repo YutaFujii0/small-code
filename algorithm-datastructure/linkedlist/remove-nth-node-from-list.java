274312f33184b4f3443f1f1d9ddbf61baddfd4e2
YutaFujii0 <y.f.mar2@gmail.com>
2022-06-23 22:28:11 +0900

    Remove nth node from end of list (java) fast/slow solution

--- algorithm-datastructure/linkedlist/remove-nth-node-from-list.java
+++ algorithm-datastructure/linkedlist/remove-nth-node-from-list.java
@@ -34,3 +34,31 @@ class Solution {
         return dummy.next;
     }
 }
+
+
+// naive solution: 
+// - first reverse linked list, 
+// - remove nth node, and reverse again, 
+// O(n) time complexity, and moreover, O(n) space complexity & fdata will be replaced
+// Two pointer approach
+// - move fast n times forward than slow
+// - move both fast/slow until fast reaches the end
+// - remove slow.next
+// - return dummy head
+// Complexity: O(n) time, O(1) space
+class Solution2 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        for(int i=0; i<n; i++) {
+            fast = fast.next;
+        }
+        while(fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
