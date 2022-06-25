6d45d8cc59c292febb0fc46891429cb11ab000d4
YutaFujii0 <y.f.mar2@gmail.com>
2022-06-25 15:41:21 +0900

    java class

---
+++ java/HelloWorld/OOP/src/com/yuta/Car.java
@@ -0,0 +1,13 @@
+package com.yuta;
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
---
+++ java/HelloWorld/OOP/src/com/yuta/Main.java
@@ -0,0 +1,11 @@
+package com.yuta;
+
+public class Main {
+    public static void main(String[] arg) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("Carrera");
+        System.out.println(porsche);
+    }
+}
