e4e549e2fc2d071a4b714583925df940d143c83c
YutaFujii0 <y.f.mar2@gmail.com>
2022-06-27 01:17:02 +0900

    java inner class

--- java/HelloWorld/Playlist/src/Album.java
+++ java/HelloWorld/Playlist/src/Album.java
@@ -4,16 +4,16 @@ import java.util.LinkedList;
 public class Album {
     private String name;
     private String artist;
-    private ArrayList<Song> songs;
+    private SongList songs;
 
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = new ArrayList<Song>();
+        this.songs = new SongList();
     }
 
     public boolean addSong(String title, double duration) {
-        if (findSong(title) != null) {
+        if (songs.findSong(title) != null) {
             return false;
         } else {
             songs.add(new Song(title, duration));
@@ -21,31 +21,57 @@ public class Album {
         }
     }
 
-    private Song findSong(String title) {
-        for (int i=0; i<songs.size(); i++) {
-            if (songs.get(i).getTitle() == title) {
-                return songs.get(i);
-            }
-        }
-        return null;
-    }
-
     public boolean addToPlayList(int index, LinkedList<Song> playlist) {
-        if (index <= 0 || songs.size() < index) {
+        Song song = songs.findSong(index);
+        if (song == null) {
             return false;
         } else {
-            playlist.add(songs.get(index - 1));
+            playlist.add(song);
             return true;
         }
     }
 
     public boolean addToPlayList(String title, LinkedList<Song> playlist) {
-        Song song = findSong(title);
+        Song song = songs.findSong(title);
         if (song != null) {
             playlist.add(song);
             return true;
         } else {
             return false;
+        }
+    }
+
+    public class SongList {
+        private ArrayList<Song> songs;
+
+        public SongList() {
+            songs = new ArrayList<Song>();
+        }
+
+        private boolean add(Song song) {
+            if (findSong(song.getTitle()) != null) {
+                return false;
+            } else {
+                songs.add(song);
+                return true;
+            }
+        }
+
+        private Song findSong(String title) {
+            for (int i=0; i<songs.size(); i++) {
+                if (songs.get(i).getTitle() == title) {
+                    return songs.get(i);
+                }
+            }
+            return null;
+        }
+
+        private Song findSong(int index) {
+            if (index <= 0 || songs.size() < index) {
+                return null;
+            } else {
+                return songs.get(index - 1);
+            }
         }
     }
 }
