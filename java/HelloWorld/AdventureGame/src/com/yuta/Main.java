3af1cc6978726559d53d2c4df7828b8ae78d1491
YutaFujii0 <y.f.mar2@gmail.com>
2022-07-07 22:17:18 +0900

    java file io

--- java/HelloWorld/AdventureGame/src/com/yuta/Main.java
+++ java/HelloWorld/AdventureGame/src/com/yuta/Main.java
@@ -4,7 +4,11 @@ public class Main {
     private static Locations locations = new Locations();
 
     public static void main(String[] args) {
-        System.out.println(locations.get(1).getDescription());
+        try {
+            System.out.println(locations.get(1).getDescription());
+        } catch (NullPointerException e) {
+            System.out.println("Detected null pointer exception.");
+        }
     }
 
     public void command() {
